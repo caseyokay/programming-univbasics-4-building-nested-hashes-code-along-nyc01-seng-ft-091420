def958e97daf24dc6cbdac38bbdf7e3c040cbde7
Casey B <caseyoksana@gmail.com>
2020-09-07 23:53:01 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -13,7 +13,7 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        Benvolio = {name: "Benvolio", age: "17", attitude: "worried"}, Mercutio = {name: "Benvolio", age: "17", attitude: "worried"}, 
+        Benvolio = {name: "Benvolio", age: "17", attitude: "worried"}, Mercutio = {name: "Mercutio", age: "18", attitude: "worried"}, 
       ]
    }
 }
